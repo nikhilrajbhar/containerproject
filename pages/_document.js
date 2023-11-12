28fcaf1335c019f98cf035c20825a25425a89444
nikhilrajbhar <nikhilrajbhar013@gmail.com>
2023-11-13 03:44:56 +0530

    minor change

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main } from "next/document";
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
@@ -10,6 +10,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+
                     <meta charSet="UTF-8" />
                     <meta name="description" content="" />
                     <meta name="author" content="" />
@@ -17,19 +18,17 @@ class MyDocument extends Document {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
                         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                    <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                        crossOrigin="anonymous"></script>
+                    <script async src="/js/jquery.min.js"></script>
+                    <script async src="/js/nav.js"></script>
                 </Head>
                 <body>
 
                     <Main />
 
-                    <NextScript />
-
                     {/* <!-- Jquery JS--> */}
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-                        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-                        crossOrigin="anonymous"></script>
-                    <script src="/js/jquery.min.js"></script>
-                    <script src="/js/nav.js"></script>
                 </body>
             </Html>
         );
